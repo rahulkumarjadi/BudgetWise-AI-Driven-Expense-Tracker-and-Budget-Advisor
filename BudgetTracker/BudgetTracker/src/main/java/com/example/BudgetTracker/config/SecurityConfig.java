b6e051accf44761e9beea612a66ea18e2b6c86ea
Rahul Kumar jadi <rahuljadi57@gmail.com>
2026-03-02 00:38:40 +0530

    Updated project files

--- BudgetTracker/BudgetTracker/src/main/java/com/example/BudgetTracker/config/SecurityConfig.java
+++ BudgetTracker/BudgetTracker/src/main/java/com/example/BudgetTracker/config/SecurityConfig.java
@@ -1,39 +1,4 @@
-//package com.example.BudgetTracker.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(
-//                                "/api/auth/**",
-//                                "/api/transactions/**",
-//                                "/api/dashboard/**",
-//                                "/api/budget/**",
-//                                "/api/savings/**",
-//                                "/api/analytics/**"
-//                        ).permitAll()
-//
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .formLogin(form -> form.disable())
-//                .httpBasic(httpBasic -> httpBasic.disable());
-//
-//        return http.build();
-//    }
-//}
-//
+
 
 
 
